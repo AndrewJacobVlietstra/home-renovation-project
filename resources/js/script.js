f564693651b882c72196d1d1a687c79691b71530
Andrew <vlietstra123@hotmail.com>
2021-01-27 16:22:26 -0500

    Added sticky navbar and fixed Jquery Issue

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,67 @@
+// Smooth Scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            // $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            // $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 // Carousel / Slideshow
 $(document).ready(function (){
     $(".carousel").slick({
         autoplay: true,
-        autoplaySpeed: 6000,
+        autoplaySpeed: 7000,
         dots: true
     });
 });
+
+// Makes an opaque navbar appear after scrolling down a certain point from the landing page
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = function() {
+    if(window.pageYOffset > 800) {
+        navbar.classList.add('sticky');
+        if(!scrolled) {
+            navbar.style.transform = 'translateY(-70px)';
+        } 
+        setTimeout(function(){
+            navbar.style.transform = 'translateY(0px)';
+            scrolled = true;
+        }, 150);
+    } else {
+        navbar.classList.remove('sticky');
+        scrolled = false;
+    }
+}
+
